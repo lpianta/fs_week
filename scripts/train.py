5647e4b5b5257452ebac38d6f58c482ddd9eedc7
lpianta <pianta.luca@gmail.com>
2021-04-20 12:09:47 +0200

    updating script and uploading flask app template

--- scripts/train.py
+++ scripts/train.py
@@ -1,7 +1,7 @@
-# import librarires
-
-import argparse
 import torch
+import json
+from pathlib import Path
+import argparse
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
@@ -10,17 +10,20 @@ from PIL import Image
 import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
+
 import time
+
 import os
 import time
+
 import copy
+
 import requests
 import zipfile
 
 
-# defining function to pass in the terminal
-
 def download_data():
+
     if "data.zip" not in os.listdir("../data"):
         r = requests.get(
             "https://github.com/polyrand/strive-ml-fullstack-public/blob/main/06_org_documentation_scripting/data.zip?raw=true"
@@ -29,7 +32,7 @@ def download_data():
         with open("../data/data.zip", "wb") as f:
             f.write(r.content)
 
-    if "hymenoptera_data" not in os.listdir("../data"):
+     if "hymenoptera_data" not in os.listdir("../data"):
         with zipfile.ZipFile("../data/data.zip", "r") as zip_ref:
             zip_ref.extractall("../data")
 
@@ -42,8 +45,7 @@ def get_dataloaders():
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [
-                                     0.229, 0.224, 0.225]),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ]
         ),
         "val": transforms.Compose(
@@ -51,8 +53,7 @@ def get_dataloaders():
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [
-                                     0.229, 0.224, 0.225]),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ]
         ),
     }
@@ -62,24 +63,33 @@ def get_dataloaders():
         x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
         for x in ["train", "val"]
     }
+
+    with open("../assets/class2idx.json", "w") as f:
+        f.write(json.dumps(image_datasets["train"].class_to_idx))
+
     dataloaders = {
         x: torch.utils.data.DataLoader(
             image_datasets[x], batch_size=4, shuffle=True, num_workers=4
         )
         for x in ["train", "val"]
     }
-
     dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "val"]}
     class_names = image_datasets["train"].classes
 
-    return {"class_names": class_names, "dataset_sizes": dataset_sizes, "dataloaders": dataloaders}
+    return {
+        "class_names": class_names,
+        "dataset_sizes": dataset_sizes,
+        "dataloaders": dataloaders,
+    }
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=10):
+
     loaders = get_dataloaders()
+
     dataloaders = loaders["dataloaders"]
     dataset_sizes = loaders["dataset_sizes"]
 
@@ -131,8 +141,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=10):
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-            print("{} Loss: {:.4f} Acc: {:.4f}".format(
-                phase, epoch_loss, epoch_acc))
+            print("{} Loss: {:.4f} Acc: {:.4f}".format(phase, epoch_loss, epoch_acc))
 
             # deep copy the model
             if phase == "val" and epoch_acc > best_acc:
@@ -151,65 +160,72 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=10):
 
     # load best model weights
     model.load_state_dict(best_model_wts)
-    torch.save(model.state_dict(), "../models/best_model.pth")
+    torch.save(model.state_dict(), "best_model.pth")
     return model
 
 
-def create_model(weights=None, epochs=5, optimizer='sgd'):
+def create_model(weights=None, epochs=5, optimizer="sgd"):
 
     model_conv = torchvision.models.resnet18(pretrained=True)
 
     for param in model_conv.parameters():
         param.requires_grad = False
 
-        # Parameters of newly constructed modules have requires_grad=True by default
+    # Parameters of newly constructed modules have requires_grad=True by default
     num_ftrs = model_conv.fc.in_features
     model_conv.fc = nn.Linear(num_ftrs, 2)
 
     model_conv = model_conv.to(device)
 
     if weights:
-        if weights not in os.listdir("../model"):
-            print("The weights do not exists in the current folder")
+        if weights not in os.listdir():
+            print("The weigths do not exists in the current folder")
+
+        model_conv.load_state_dict(weights)
 
     criterion = nn.CrossEntropyLoss()
 
     # Observe that only parameters of final layer are being optimized as
     # opposed to before.
-    if optimizer == 'sgd':
-        optimizer_conv = optim.SGD(
-            model_conv.fc.parameters(), lr=0.001, momentum=0.9)
 
-    elif optimizer == 'adam':
+    if optimizer == "sgd":
+
+        optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+    elif optimizer == "adam":
+
         optimizer_conv = optim.Adam(
-            model_conv.fc.parameters(), lr=0.001, weight_decay=0.01, eps=1e-5)
+            model_conv.fc.parameters(), lr=0.001, weight_decay=0.01, eps=1e-5,
+        )
     else:
-        print("Sorry I can't use that optimizer")
+        print("Sorry I can't use that optmizer")
         return
 
-    print(f"Using optimizer: {optimizer_conv}")
+    print(f"Using optmizer: {optimizer_conv}")
 
     # Decay LR by a factor of 0.1 every 7 epochs
-    exp_lr_scheduler = lr_scheduler.StepLR(
-        optimizer_conv, step_size=3, gamma=0.1)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=3, gamma=0.1)
 
     start = time.perf_counter()
 
     model_conv = train_model(
-        model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=epochs)
+        model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=epochs
+    )
 
     total = time.perf_counter() - start
 
     print(f"Total time {total}")
 
-    torch.save(model_conv, "../models/model.pkl")
-
 
 def main(args):
+
     print(args)
 
+    if args.get_dls:
+        get_dataloaders()
+
     if args.download_data:
-        print("Downloading data")
+        print("downloading data")
         download_data()
 
     if args.train:
@@ -217,23 +233,26 @@ def main(args):
             epochs = args.epochs
         else:
             epochs = 2
+
         if args.optimizer:
             optimizer = args.optimizer
         else:
-            optimizer = 'sgd'
+            optimizer = "sgd"
 
-    create_model(args.load_weights, epochs=epochs, optimizer=optimizer)
+        create_model(args.load_weights, epochs=epochs, optimizer=optimizer)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--download-data',
-                        help="Download the standard data", action='store_true')
-    parser.add_argument('--train', help='Train the model', action='store_true')
-    parser.add_argument('--epochs', type=int, help='Number of epochs')
-    parser.add_argument('--optimizer', type=str, help='Optimizer to use')
-    parser.add_argument('--load-weights', action='store_true')
+    parser.add_argument("--download-data", action="store_true")
+    parser.add_argument("--get-dls", action="store_true")
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--epochs", type=int, help="number of epochs to train")
+    parser.add_argument("--optimizer", type=str)
+    parser.add_argument("--load-weights", action="store_true")
 
     args = parser.parse_args()
 
     main(args)
+    exit()
