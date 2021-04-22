1dfa5025a46e703b9efee99ee38095e05ad2675d
lpianta <pianta.luca@gmail.com>
2021-04-22 12:40:59 +0200

    changes to some files and gitignore

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# fs_week
+# Short product title
+
+> Short explanation about it
+
+## Managing dependencies and installation
+
+We are using [pip-tools](https://github.com/jazzband/pip-tools)
+
+Make sure to install pip-tools **in the same environment** you are working on.
+
+```
+pip install pip-tools
+```
+
+To build the `.txt` files use:
+
+```bash
+pip-compile -v --output-file requirements/production.txt requirements/production.in
+pip-compile -v --output-file requirements/dev.txt requirements/dev.in
+```
+
+To sync the dependencies use:
+
+```bash
+pip-sync requirements/*.txt
+```
+
+If you are using a single file called `requirements.txt` (maybe easier at first), use it like:
+
+```bash
+pip-compile -v --output-file requirements/requirements.txt requirements/requirements.in
+pip-sync requirements/requirements.txt
+```
+
+## Testing the app
+
+Testing is done using [pytest](https://docs.pytest.org/en/6.2.x/). After you install it, from the root of the repo run:
+
+```bash
+python3 -m pytest -vv
+```
+
+## Running the Flask app
+
+TO DO
+
+## Training a new model
+
+TO DO
+
+## Contributing to the docs
+
+We are using [mkdocs](https://www.mkdocs.org/)
+
+Documentation is deployed using the command `mkdocs gh-deploy` do not
+update the files in GitHub directly. Update the markdown in your laptop and use the command.
+Also, remember to keep the `mkdocs.yaml` synchronized with your markdown files.
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,23 @@
-from app.app import get_hash, verify_hash
+from app.app import get_hash, verify_hash, DB
+import os
 import re
 import pytest
-import sqlite3
 
 
 def test_get_hash():
+
+    # random test
+
+    # make sure the hash of a string is not the same as the string
     assert get_hash("hello") != "hello"
     assert get_hash("strive") != "strive"
 
 
 def test_bcrypt():
+
+    # check that a hash matches the bcrypt patter (at least if using passlib)
+    # *NOTICE* if you don't have passlib, app.py will use another hashing system
+    # and this test will fail
 
     bcrypt_hash_pattern = r"^\$2[ayb]\$.{56}$"
     pat = re.compile(bcrypt_hash_pattern)
@@ -28,13 +36,49 @@ def test_hash_computation(testing_string):
 
 
 @pytest.mark.parametrize(
-    "email,password", [("hello@hello.com", "asd"),
-                       ("strive@strive.com", "asdasd")]
+    "email,password", [("hello@hello.com", "asd"), ("strive@strive.com", "asdasd")]
 )
 def test_user_creation(email, password):
+
+    # We are not checking if an email already exists so
+    # we can have the same email but *the password hashes
+    # will be different*. For that reason, I'm deleting the whole
+    # users table on every new test
+
+    # BE CAREFUL: If I were running this test on a "production" database, it would break
+    # everything. That's why I'm creating a new db called "test.db" for every test.
+
+    db = DB(dbname="testing.db")
+
+    # remove eerything from the users table
+    db.conn.execute("DELETE FROM users")
+    db.conn.execute("COMMIT")
+
+    # create a new user (password will be hashed inside the function)
+    user_id = db.create_user(email, password)
+
+    # here I'm using the internal DB connection in the class to send a custom
+    # SQL query
+    # I'm doing this to get the email and the hashed password of the user we just
+    # created. The email should be the same one we have used in the
+    # db.create_user() function
+    result = db.conn.execute(
+        "SELECT * from users where user_id = ?", (user_id,)
+    ).fetchone()
+
+    # result is a tuple with 3 elements:
+    # (user_id, email, hashed_password)
+
+    user_id = result[0]
+    new_user_email = result[1]
+    hashed_password = result[2]
+
+    # make sure the emails match
+    assert new_user_email == email
+
+    assert db.validate_password(email, password)
+
+    # examples of what to test
     # 1. verify user is created
-    conn = sqlite3.connect("app/ml_app.db", check_same_thread=False)
-    with conn as c:
-        assert c.execute(
-            "SELECT * FROM users WHERE email = ? and password = ?", (email, get_hash(password)))
     # 2. verify you can validate the hash
+    # 3. whatever you come up with
